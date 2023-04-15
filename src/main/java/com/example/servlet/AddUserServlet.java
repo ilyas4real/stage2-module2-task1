8cf8d62b374156824cfc3e469ce50dc580561174
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-04-15 14:19:59 +0200

    added annotations

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+@WebServlet("/add")
 public class AddUserServlet extends HttpServlet{
     //write your code here!
     @Override
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -5,12 +5,14 @@ import com.example.Warehouse;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Set;
 
+@WebServlet("/users")
 public class GetUsersServlet extends HttpServlet {
     //write your code here!
     @Override
